fdd27c0ab23ffc977bb0920bd6078f58f6f6215e
Lemon <riegoshinandrei@gmail.com>
2021-06-24 15:53:21 +0800

    Changed implementation of schema conversion

--- src/utils.js
+++ src/utils.js
@@ -3,28 +3,21 @@ const uniqid = () => Math.random().toString(36).substr(2, 5);
 const convertFormGroupSchema = (fields) => {
   const obj = { id: uniqid(), fields: {} };
 
-  fields.forEach((field) => {
-    obj.fields[field.name] = {};
-    obj.fields[field.name].value = '';
-    obj.fields[field.name].error = false;
-    obj.fields[field.name].schema = field;
+  Object.entries(fields).forEach(([name, schema]) => {
+    obj.fields[name] = {
+      value: schema.defaultValue || '',
+      error: false,
+      errorMsg: '',
+    };
   });
-
-  return obj;
 };
 
 const convertFormBlockSchema = (schema) => {
   const obj = {};
 
-  schema.forEach((block) => {
-    const id = uniqid();
-
-    obj[block.name] = { id };
-    obj[block.name].groups = [convertFormGroupSchema(block.fields)];
-    obj[block.name].schema = block;
+  Object.entries(schema).forEach(([name, schema]) => {
+    obj[name] = { groups: [convertFormGroupSchema(schema.fields)] };
   });
-
-  return obj;
 };
 
 const capitalizeEach = (type, str) => {
