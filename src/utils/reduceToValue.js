591ddeb867d7166f2fac68486f0a0ce9bd0d185d
Lemon <riegoshinandrei@gmail.com>
2021-07-07 22:13:16 +0800

    Added date formatting

--- src/utils/reduceToValue.js
+++ src/utils/reduceToValue.js
@@ -1,9 +1,14 @@
+import isDate from 'date-fns/isDate';
+import format from 'date-fns/format';
+
 export const reduceToValue = (data) =>
   Object.entries(data).reduce((reducedData, [name, blockData]) => {
     reducedData[name] = blockData.groups
       .map((group) =>
         Object.entries(group.fields).reduce((groupData, [field, value]) => {
-          groupData[field] = value.value.trim() || '';
+          groupData[field] = isDate(value.value)
+            ? format(value.value, 'MMM. d, y')
+            : value.value.trim() || '';
 
           return groupData;
         }, {})
